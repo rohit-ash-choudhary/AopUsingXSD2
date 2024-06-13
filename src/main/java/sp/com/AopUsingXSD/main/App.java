ba5f982638408135440d5d600164594000fd34f3
rohit-ash-choudhary <Rooh@#2001S>
2024-06-13 14:25:36 +0530

    Aop Using XSD for any specific method

---
+++ src/main/java/sp/com/AopUsingXSD/main/App.java
@@ -0,0 +1,30 @@
+package sp.com.AopUsingXSD.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import sp.com.AopUsingXSD.services.BankTransaction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        ApplicationContext context= new ClassPathXmlApplicationContext("/sp/com/AopUsingXSD/resources/ApplicationContext.xml");
+     
+        
+        BankTransaction bt= (BankTransaction) context.getBean("objServices");
+        bt.Upipayment();
+        System.out.println();
+        bt.NetbackingPayement();
+        System.out.println();
+        bt.WalletPayment();
+     
+       
+    }
+}
